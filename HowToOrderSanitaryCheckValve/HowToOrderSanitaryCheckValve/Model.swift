e0d1a157c8ce4ee808874c1e0452a4b2f3e6ab12
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-22 22:30:01 -0600

    Add Model swift file

---
+++ HowToOrderSanitaryCheckValve/HowToOrderSanitaryCheckValve/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  HowToOrderSanitaryCheckValve
+//
+//  Created by Carlos Santiago Cruz on 12/22/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import Foundation
